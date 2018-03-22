27285e3fcc6abb33b650b5c9d67f3921612c7bcb
Christilyn A <christilynarjona@outlook.com>
2018-03-22 17:39:38 -0600

    references & declarations count

--- src/CountingVisitor.java
+++ src/CountingVisitor.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.*;
 
 public class CountingVisitor extends org.eclipse.jdt.core.dom.ASTVisitor {
 
@@ -27,18 +27,58 @@ public class CountingVisitor extends org.eclipse.jdt.core.dom.ASTVisitor {
         return types;
     }
     
-    //ok gotta do the visits now
+    private boolean checkDeclarations(AbstractTypeDeclaration node) {
+        String name = node.resolveBinding().getQualifiedName();
+        if(types.contains(name)) {
+            int i = types.indexOf(name);
+            counts.get(i)[0]++;
+        }
+        else {
+            types.add(name);
+            counts.add(new int[] {1, 0});
+        }
+        return true;
+    }
+
+    /////////////////////// declarations count ///////////////////////////
     
     public boolean visit(TypeDeclaration node) {
-    	String name = node.resolveBinding().getQualifiedName();
-    	if(types.contains(name)) {
-    		int i = types.indexOf(name);
-    		counts.get(i)[0]++;
-    	}
-    	else {
-    		types.add(name);
-    		counts.add(new int[] {1, 0});
-    	}
-    	return true;
+    	return checkDeclarations(node);
+    }
+
+    @Override
+    public boolean visit(AnnotationTypeDeclaration node) {
+        return checkDeclarations(node);
+    }
+
+    @Override
+    public boolean visit(EnumDeclaration node) {
+        return checkDeclarations(node);
+    }
+
+    /////////////////////// references count ///////////////////////////
+
+    @Override
+    public boolean visit(SimpleName node) {
+        String name = node.getFullyQualifiedName();
+        if(types.contains(name)){
+            int i = types.indexOf(name);
+            // TODO increment both reference & declaration?
+            counts.get(i)[0]++;
+            counts.get(i)[1]++;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean visit(PrimitiveType node) {
+        String name = node.getPrimitiveTypeCode().toString();
+        if(types.contains(name)){
+            int i = types.indexOf(name);
+            // TODO increment both reference & declaration?
+            counts.get(i)[0]++;
+            counts.get(i)[1]++;
+        }
+        return true;
     }
 }
